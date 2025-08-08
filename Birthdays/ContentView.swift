0423b3217a539be1c1e22763c5f67b7e34ba8f32
Ainslea Hong <ainslea.hong2028@gmail.com>
2025-08-08 10:48:19 -0400

    C and R operation

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -10,20 +10,48 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var friends: [Friend] = [
-        Friend (name: "Chloe", birthday: .now),
-        Friend (name: "Nusayba", birthday: Date(timeIntervalSince1970: 0))
+        Friend(name: "Chloe", birthday: .now),
+        Friend(name: "Nusayba", birthday: .now)
     ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     
     var body: some View {
-        List(friends, id: \.name) { friend in HStack{
-            Text(friend.name)
-            Spacer()
-            Text(friend.birthday, format: .dateTime.month(.wide).day().year())
-        }
-        }
-    }
+        NavigationStack {
+            
+            List(friends, id: \.name) { friend in HStack{
+                Text(friend.name)
+                Spacer()
+                Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+            }//end of hstack
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom){
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date){
+                        TextField("Name", text:$newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend =
+                        Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
+                
+            }//end of navigation stack
+        }//end of body
+    }//end of struct
 }
-
-#Preview {
-    ContentView()
+    #Preview {
+        ContentView()
+    
 }
